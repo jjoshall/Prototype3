3a1c936307e5293e841e8a4fd231315503852dee
nikolai.dil <nick.dilo55@gmail.com>
2024-11-06 21:29:02 -0800

    Camera
    Added camera flash, camera crosshair, first person camera

---
+++ Prototype 3/Assets/Scripts/Picture Capture.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Picture : MonoBehaviour
+{
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(FlashEffect());
+        }
+    }
+
+    private IEnumerator FlashEffect()
+    {
+        // Enable the flash effect
+        cameraFlash.SetActive(true);
+        
+        // Wait for the specified flash time
+        yield return new WaitForSeconds(flashTime);
+        
+        // Disable the flash effect
+        cameraFlash.SetActive(false);
+    }
+}
